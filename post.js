d668f75354e61adee224ac451415ba708b5749f0
DanBur1 <119559983+DanBur1@users.noreply.github.com>
2024-05-23 21:21:44 +0700

    Delete post.js

--- post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-    id: { type: Number, required: true, unique: true },
-    title: { type: String, required: true, unique: true },
-    text: { type: String, required: true },
-    author: { type: Number, required: true}
-});
-const Post = mongoose.model('Post', UserSchema);
-module.exports = Post;
